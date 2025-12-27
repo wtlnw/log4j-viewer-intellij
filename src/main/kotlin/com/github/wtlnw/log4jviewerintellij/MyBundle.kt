a5f35cb9f2aee4f7c71d0b484f06b26f7f20970c
GitHub Action <action@github.com>
2025-12-27 13:48:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/wtlnw/log4jviewerintellij/MyBundle.kt
+++ src/main/kotlin/com/github/wtlnw/log4jviewerintellij/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.wtlnw.log4jviewerintellij
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/wtlnw/log4jviewerintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wtlnw/log4jviewerintellij/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.wtlnw.log4jviewerintellij.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wtlnw.log4jviewerintellij.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
